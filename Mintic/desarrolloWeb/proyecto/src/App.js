31a54c0b0f2aff33d7144f383cb9121432893c14
brincon <brendarin10@gmail.com>
2023-03-20 12:28:10 -0500

    agregar cambios con todos los elmentos y estilos de la pagina de los perritos

--- Mintic/desarrolloWeb/proyecto/src/App.js
+++ Mintic/desarrolloWeb/proyecto/src/App.js
@@ -1,6 +1,7 @@
-import logo from './media/logo.png';
 import './styles/styles.css';
-import borderCollie from './media/borderCollie.png'
+import logo from './media/logo.png';
+import borderCollie from './media/borderCollie.jpg';
+import rhodesian from './media/rhodesian.jpg';
 
 function App() {
   return (
@@ -8,23 +9,39 @@ function App() {
           <header>
         <ul className="navbar">
             <li>
-                <img src={logo} alt="image" className="logo" />
+                <img src={logo} alt="image" className="logo"/>
             </li>
-            <li><button className="button mainButton">Nuevo post</button></li>
+            <li><button className="botonGenerico mainButton">Nuevo post</button></li>
             <li>
                 <div className="buscar">
                     <input placeholder="Buscar una raza"/>
-                    <i className="fa-solid fa-magnifying-glass"></i>
+                    <i className="fa-solid fa-magnifying-glass botonGenerico iconoBusqueda"></i>
                 </div>
             </li>
-            <li><button className="button secundaryButton">login</button></li>
-            <li><button className="button mainButton">Registro</button></li>
+            <li><button className="botonGenerico secundaryButton">login</button></li>
+            <li><button className="botonGenerico mainButton">Registro</button></li>
             
          
         </ul>
     </header>
     <main>
-        <section></section>
+        <section>
+            <h1>Razas de perros</h1>
+            <ul className="breedCardContainer">
+                <li className="breedCard">
+                    <div className="contenedorImagen"> 
+                        <img src={borderCollie} alt="Border Collie"/>
+                    </div> 
+                    <span className="breedTitle">Border Collie </span> 
+                </li>
+                <li className="breedCard">
+                    <div className="contenedorImagen"> 
+                        <img src={rhodesian} alt="Rhodesian"/>
+                    </div>
+                    <span className="breedTitle">Rhodesian </span>
+                </li>
+            </ul>
+        </section>
         <section></section>
     </main>
     <footer></footer>
