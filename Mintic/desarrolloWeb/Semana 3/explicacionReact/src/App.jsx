f873df4b732c956810cda1e79f848637aa57dadb
brincon <brendarin10@gmail.com>
2023-03-21 17:39:34 -0500

    crear carpeta explicacion react y crear card raza de perros en una funcion archivo app.jsx

---
+++ Mintic/desarrolloWeb/Semana 3/explicacionReact/src/App.jsx
@@ -0,0 +1,53 @@
+import './styles/styles.css';
+import logo from './media/logo.png';
+import borderCollie from './media/borderCollie.jpg';
+import rhodesian from './media/rhodesian.jpg';
+
+function App() {
+  return (
+    <div className="App">
+          <header>
+        <ul className="navbar">
+            <li>
+                <img src={logo} alt="image" className="logo"/>
+            </li>
+            <li><button className="botonGenerico mainButton">Nuevo post</button></li>
+            <li>
+                <div className="buscar">
+                    <input placeholder="Buscar una raza"/>
+                    <i className="fa-solid fa-magnifying-glass botonGenerico iconoBusqueda"></i>
+                </div>
+            </li>
+            <li><button className="botonGenerico secundaryButton">login</button></li>
+            <li><button className="botonGenerico mainButton">Registro</button></li>
+            
+         
+        </ul>
+    </header>
+    <main>
+        <section>
+            <h1>Razas de perros</h1>
+            <ul className="breedCardContainer">
+                <CardRazasPerros nombreRaza="Border Collie" imagen ={borderCollie}/>
+                <CardRazasPerros nombreRaza="Rhodesian" imagen ={rhodesian}/>           
+            </ul>
+        </section>
+        <section></section>
+    </main>
+    <footer></footer>
+    </div>
+  );
+}
+function CardRazasPerros({nombreRaza, imagen}){
+    return (
+        <li className="breedCard">
+        <div className="contenedorImagen"> 
+            <img src={imagen} alt={nombreRaza}/>
+        </div>
+        <span className="breedTitle">{nombreRaza} </span>
+    </li>
+    );
+
+}
+
+export default App;
