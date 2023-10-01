df2312ce54a6fac104e5183a11a6352190ca6eb0
brincon <brendarin10@gmail.com>
2023-10-01 16:36:17 -0500

    Se agrega la clase y el reto dia 6 directorios y mis recetas

--- Udemy/Python/directorios.py
+++ Udemy/Python/directorios.py
@@ -20,17 +20,26 @@ archivo = carpeta / 'prueba.txt'
 # mi_archivo = open(archivo)
 # print(mi_archivo.read())
 
+# sobreescribe
+# mi_archivo = open(archivo, w)
+# print(mi_archivo.write("Hola"))
+
+# linea al final
+# mi_archivo = open(archivo, a)
+# print(mi_archivo.write("Hola"))
+
+
 carpeta = Path('C:/Users/brend/OneDrive/Documentos/GitHub/pythonbrenda/Udemy/Python/text.txt')
 # lee el contenido
 print(carpeta.read_text())
 
-# nombre del archivo
+# nombre del archivo completo
 print(carpeta.name)
 
 # nombre del archivo .txt
 print(carpeta.suffix)
 
-# nombre del archivo text
+# nombre del archivo text sin extensión
 print(carpeta.stem)
 
 # nombre del archivo text
@@ -42,3 +51,23 @@ else:
 ruta_windows = PureWindowsPath(carpeta)
 
 print(ruta_windows)
+
+## base 
+base = Path.home()
+# concatena en ruta y se puede un path dentro de otro
+guia = Path("Barcelona", "Europa", Path("libro.txt"))
+print(base)
+print(guia.parent.parent)
+
+#revisar todas las rutas
+guia = Path(Path.home(), "Europa")
+### para todos el directorio guia y sus subdirectorios
+for txt in Path(guia).glob("**/*.txt"):
+    print(txt)
+
+## también rutas relativas
+guia = Path("Europa", "españa", "Barcelona", "libro.txt")
+en_europa = guia.relative_to(Path("Europa"))
+en_espania = guia.relative_to(Path("Europa","España"))
+print(en_europa)
+print(en_espania)
